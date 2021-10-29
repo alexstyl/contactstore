8a808fd7fc54dcce4a1a2becfe6977b8e38b232f
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2021-10-29 23:29:51 +0100

    Update sample app with details

--- sample/src/main/java/com/alexstyl/contactstore/sample/ui/theme/Theme.kt
+++ sample/src/main/java/com/alexstyl/contactstore/sample/ui/theme/Theme.kt
@@ -1,17 +1,9 @@
 package com.alexstyl.contactstore.sample.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
     primary = Purple500,
@@ -22,17 +14,10 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun SampleAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content

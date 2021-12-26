1dd763697e385ba50abbb396553e9a87bbf9d918
Remy Benza <47558082+rbenza@users.noreply.github.com>
2021-12-26 07:55:43 +0100

    Fix for ANR. Use trySend and check for channel closure in uriFlow extension

--- library/src/main/java/com/alexstyl/contactstore/UriFlow.kt
+++ library/src/main/java/com/alexstyl/contactstore/UriFlow.kt
@@ -5,8 +5,8 @@ import android.database.ContentObserver
 import android.net.Uri
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
@@ -14,7 +14,10 @@ internal fun ContentResolver.uriFlow(uri: Uri): Flow<Unit> {
     return callbackFlow {
         val observer = object : ContentObserver(Handler(Looper.getMainLooper())) {
             override fun onChange(selfChange: Boolean) {
-                trySendBlocking(Unit)
+                if (!channel.isClosedForSend) {
+                    Log.d("~!", "onChange URI FLOW triggered: ${uri}")
+                    trySend(Unit)
+                }
             }
         }
         registerContentObserver(uri, true, observer)

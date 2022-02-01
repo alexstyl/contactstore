8ebbca7a17b00305f68ccfb735aeeaa8efd78155
Alex Styl <1665273+alexstyl@users.noreply.github.com>
2022-02-01 17:15:39 +0000

    Prevent passing of id in the LabeledValue's constructor

--- library/src/main/java/com/alexstyl/contactstore/LabeledValue.kt
+++ library/src/main/java/com/alexstyl/contactstore/LabeledValue.kt
@@ -1,10 +1,12 @@
 package com.alexstyl.contactstore
 
-public data class LabeledValue<T>(
+public data class LabeledValue<T> internal constructor(
     val value: T,
     val label: Label,
-    val id: Long? = null,
-) where T : Any
+    val id: Long?,
+) where T : Any {
+    public constructor(value: T, label: Label) : this(value, label, null)
+}
 
 internal fun <T : Any> LabeledValue<T>.requireId(): Long {
     return requireNotNull(id) {
